df63b94234f0dd0755c5100872d028284e8e2546
opilane <nikita.rimitsen@gmail.com>
2021-12-01 12:34:01 +0200

    1

--- content/kommentaar.php
+++ content/kommentaar.php
@@ -1,143 +1,24 @@
-
 <?php
-//require('conf.php');
-/*session_start();
-if (!isset($_SESSION['tuvastamine'])){//!-не / ne
-    header('Location: loginAB.php');
-    exit();
-
-}
+require ('conf.php'); //require - запрашиваем
 function clearVarsExcept($url, $varname){
     return strtok(basename($_SERVER['REQUEST_URI']), "?")."?$varname=".$_REQUEST[$varname];
 }
 
 global $yhendus;
 //lisamine INSERT INTO
-if(!empty($_REQUEST['koomentaartabel'])){
+if(!empty($_REQUEST['kommentnimi'])){
     $kask=$yhendus->prepare('INSERT INTO kommentaar(Kommentaar, Hinne, Nimi, Perenimi)
 VALUES (?, ?, ?, ?)');
-//"s" - string
-// $_REQUEST['loomanimi'] - запрос в текстовый ящик input name="loomanimi"
-    $kask->bind_param("ssss", $_REQUEST['koomentaartabel'], $_REQUEST['hinne'], $_REQUEST['nimed'], $_REQUEST['perenimed']);
+
+    $kask->bind_param("ssss", $_REQUEST['kommentnimi'], $_REQUEST['hinn'], $_REQUEST['nim'], $_REQUEST['perenim']);
     $kask->execute();
 // изменяет адресную строку
     //$_SERVER[PHP_SELF] - до имени файла
     header("Location:" .basename($_SERVER['REQUEST_URI']));
 
 }
-
-
-
-// puu kustutamine - uus
-if (isset($_REQUEST['kustuta'])){
-    $kask=$yhendus->prepare("DELETE FROM loomad WHERE id=?");
-    $kask->bind_param("i",$_REQUEST['kustuta'] );
-    $kask->execute();
-}
-
-
-
-
-?>
-<!DOCTYPE html>
-<html lang="et">
-<head>
-    <link rel="stylesheet" type="text/css" href="../style/style.css">
-    <meta charset="UTF-8">
-    <title>Kommentaar</title>
-</head>
-<body>
-<div>
-    <p><?$_SESSION["kasutaja"]?> on sisse logitud</p>
-    <form action="logout.php" method="post">
-        <input type="submit" value="Logi välja" name="logout2">
-    </form>
-</div>
-<div class ="leftcolumn">
-    <h2>Kommentaar</h2>
-
-    <a href="https://rimitsen20.thkit.ee/phpLeht/index.php?">Tagasi</a>
-    <br>
-    <?php
-global $yhendus;
-//tabeli sisu näitamine
-$kask=$yhendus->prepare("SELECT KommentaarID, Kommentaar, Hinne, Nimi, Perenimi FROM kommentaar");
-$kask->bind_result($id, $komment, $hinne, $nimi, $perenimi);
-$kask->execute();
-echo "<table>";
-echo "<tr>
-<th>ID</th>
-<th>Kommentaar</th>
-<th>Hinne</th>
-<th>Nimi</th>
-<th>Perenimi</th>
-</tr>";
-//fetch() - извлечение данных из набора данных
-    while($kask->fetch()){
-        echo "<tr>";
-        echo "<td>$id</td>";
-        echo "<td>$komment</td>";
-        echo "<td>$hinne</td>";
-        echo "<td>$nimi</td>";
-        echo "<td>$perenimi</td>";
-        echo "<td><a href='".clearVarsExcept(basename($_SERVER['REQUEST_URI']), "leht")."&kustuta=$id'>Kustuta</a></td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-
-    ?>
-</div>
-<br><br>
-<div>
-    <form action="<?=clearVarsExcept(basename($_SERVER['REQUEST_URI']), "leht")?>" method="post">
-        <label for="koomentaartabel">Loomanimi</label>
-        <br>
-        <input type="text" name="koomentaartabel" id="koomentaartabel">
-        <br>
-        <label for="hinne">Kirjeldus</label>
-        <br>
-        <label for="nimed">Kirjeldus</label>
-        <br>
-        <label for="perenimed">Kirjeldus</label>
-        <br>
-        <input type="text" name="hinne" id="hinne">
-        <br><br>
-        <input type="submit" value="Lisa">
-    </form>
-
-
-</body>
-</html>
-*/
-
-
-require ('conf.php'); //require - запрашиваем
-// функция, которая удаляет из адресной строки переменные
-function clearVarsExcept($url, $varname){
-    return strtok(basename($_SERVER['REQUEST_URI']), "?")."?$varname=".$_REQUEST[$varname];
-}
-
-global $yhendus;
-//lisamine INSERT INTO
-if(!empty($_REQUEST['kommentaarnimi'])){
-    $kask=$yhendus->prepare('INSERT INTO kommentaar(nimi, kirjeldus)
-VALUES (?, ?)');
-//"s" - string
-// $_REQUEST['kommentaarnimi'] - запрос в текстовый ящик input name="kommentaarnimi"
-    $kask->bind_param("ss", $_REQUEST['kommentaarnimi'], $_REQUEST['kirj']);
-    $kask->execute();
-// изменяет адресную строку
-    //$_SERVER[PHP_SELF] - до имени файла
-    header("Location:" .basename($_SERVER['REQUEST_URI']));
-
-}
-
-//lisamine kirjeldus INSERT INTO
-
-
-//tabelist kustutamine
 if(isset($_REQUEST['kustuta'])){
-    $kask=$yhendus->prepare('DELETE FROM loomad WHERE id=?');
+    $kask=$yhendus->prepare('DELETE FROM kommentaar WHERE KommentaarID=?');
     $kask->bind_param("i",$_REQUEST['kustuta']);
     $kask->execute();
 }
@@ -147,14 +28,15 @@ if(isset($_REQUEST['kustuta'])){
 <!DOCTYPE html>
 <html lang="et">
 <head>
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
     <meta charset="UTF-8">
     <title>Kommentaar</title>
 </head>
 <body>
 <h1>
-    Andmetabeli "Kommentaar" sisu näitamine
+    Andmetabeli "Koomentaar" sisu näitamine
 </h1>
-<div ">
+<div >
 <?php
 global $yhendus;
 //tabeli sisu näitamine
@@ -163,17 +45,14 @@ $kask->bind_result($id, $komment, $hinne, $nimi, $perenimi);
 $kask->execute();
 echo "<table>";
 echo "<tr>
-<th>ID</th>
+<th>id</th>
 <th>Kommentaar</th>
 <th>Hinne</th>
 <th>Nimi</th>
 <th>Perenimi</th>
 </tr>";
-
-
 //fetch() - извлечение данных из набора данных
 while($kask->fetch()){
-    echo "<tr>";
     echo "<td>$id</td>";
     echo "<td>$komment</td>";
     echo "<td>$hinne</td>";
@@ -185,25 +64,32 @@ while($kask->fetch()){
 echo "</table>";
 
 ?>
-</div>
-<!--new code-->
-<br><br>
-<div>
-    <form action="<?=clearVarsExcept(basename($_SERVER['REQUEST_URI']), "leht")?>" method="post">
-        <label for="kommentaarnimi">Loomanimi</label>
-        <br>
-        <input type="text" name="kommentaarnimi" id="kommentaarnimi">
-        <br>
-        <label for="kirj">Kirjeldus</label>
-        <br>
-        <input type="text" name="kirj" id="kirj">
-        <br><br>
-        <input type="submit" value="Lisa">
-    </form>
-</div>
 
-<?php
-$yhendus->close();
-?>
+    <div>
+        <form action="<?=clearVarsExcept(basename($_SERVER['REQUEST_URI']), "leht")?>" method="post">
+            <label for="kommentnimi">Kommentaar</label>
+            <br>
+            <input type="text" name="kommentnimi" id="kommentnimi">
+            <br>
+
+            <label for="hinn">Hinne</label>
+            <br>
+            <input type="text" name="hinn" id="hinn">
+            <br><br>
+
+            <label for="nim">Nimi</label>
+            <br>
+            <input type="text" name="nim" id="nim">
+            <br><br>
+
+            <label for="perenim">Perenimi</label>
+            <br>
+            <input type="text" name="perenim" id="perenim">
+            <br><br>
+
+            <input type="submit" value="Lisa">
+        </form>
+    </div>
+</div>
 </body>
 </html>
